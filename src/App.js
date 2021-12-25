d8fabfe227277e0924523d8d95dd648ea8120a9f
Ameybairolu <ameybairolu@gmail.com>
2021-12-25 11:21:50 +0530

    installed chatengine and have set it up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="e85d0f8e-1def-44bf-aa47-4ee395b7626a"
+            userName="ameybairolu"
+            userSecret="1234"
+        />
+    )
+}
+
+export default App;
