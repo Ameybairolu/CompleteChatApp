dcb0f6d24c1cbba2085b68c90ffa8b3c5e0a31cc
Ameybairolu <ameybairolu@gmail.com>
2021-12-30 16:58:15 +0530

    made major changes 36:16

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
             projectID="e85d0f8e-1def-44bf-aa47-4ee395b7626a"
             userName="ameybairolu"
             userSecret="1234"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
 }
---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,4 @@
+const MessageForm = ({ message }) => {
+  return <div>MessageForm</div>;
+};
+export default MessageForm;
