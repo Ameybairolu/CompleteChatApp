e942c0a65b94610451009f6c5911bb9140c3b066
Ameybairolu <ameybairolu@gmail.com>
2022-01-02 14:13:58 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />
     return (
         <ChatEngine
             height="100vh"
             projectID="e85d0f8e-1def-44bf-aa47-4ee395b7626a"
-            userName="ameybairolu"
-            userSecret="1234"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
